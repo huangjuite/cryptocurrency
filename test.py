8da985548dfd71672c7a3b935f305da569ac03f2
huangjuite <huangjuite@gmail.com>
2021-02-04 18:41:14 +0800

    mpf

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from binance.client import Client
 import config
+import mplfinance as mpf
+import numpy as np
+import pandas as pd
+import datetime
 
 client = Client(config.API_KEY, config.API_SECRET)
 
@@ -16,7 +20,25 @@ order = client.create_test_order(
 
 
 klines = client.get_klines(
-    symbol="ETHUSDT", interval=client.KLINE_INTERVAL_1MINUTE)
+    symbol="ETHUSDT", interval=client.KLINE_INTERVAL_30MINUTE, limit=100)
 
-for k in klines:
-    print(k)
+
+klines = np.array(klines)
+klines = klines[:, :6]
+
+
+data = pd.DataFrame(data=klines, columns=[
+                    "Time", "Open", "High", "Low", "Close", "Volume"])
+data["Time"] = data["Time"].astype('int')/1000
+data["Open"] = data["Open"].astype('float')
+data["High"] = data["High"].astype('float')
+data["Low"] = data["Low"].astype('float')
+data["Close"] = data["Close"].astype('float')
+data["Volume"] = data["Volume"].astype('float')
+
+data["Time"] = [datetime.datetime.fromtimestamp(x) for x in data["Time"]]
+data.reset_index(drop=True, inplace=True)
+data.set_index('Time', inplace=True)
+print(data)
+
+mpf.plot(data, type='candle', mav=(6,9), volume=True)
